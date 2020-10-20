a69e738a73a79d2b78ef0444a66cf603b742efeb
Virginia Baroni <virg.baroni@gmail.com>
2020-10-20 14:02:43 +0200

    continuously saves the game amount

--- Assets/Scripts/GoldManager.cs
+++ Assets/Scripts/GoldManager.cs
@@ -1,31 +1,33 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class GoldManager : MonoBehaviour
-{
-    public int goldAmount;
-
-    public Text goldAmountText;
+public class GoldManager : MonoBehaviour {
     public int goldAmountPerClick = 5;
+    public Text goldAmountText;
 
-    void Start()
-    {
-        this.goldAmount = PlayerPrefs.GetInt("Gold", 1);
-    }
-    
-    void OnDestroy() {
-        PlayerPrefs.SetInt("Gold", this.goldAmount);
+    public int GoldAmount {
+        get => PlayerPrefs.GetInt("Gold", 1);
+        set {
+            PlayerPrefs.SetInt("Gold", value);
+            UpdateGoldAmountLabel();
+        }
     }
 
-    void Update()
-    {
-        this.goldAmountText.text = this.goldAmount.ToString("0 Gold");
+    void UpdateGoldAmountLabel() {
+        this.goldAmountText.text = this.GoldAmount.ToString("0 Gold");
+    }
+
+    void Start() {
+        UpdateGoldAmountLabel();
+    }
+	
+    void Update() {
         if (Input.GetMouseButtonDown(0)) {
             ProduceGold();
         }
     }
-    
+
     public void ProduceGold() {
-        this.goldAmount += goldAmountPerClick; // this.goldAmount = this.goldAmount + 5;
+        this.GoldAmount += this.goldAmountPerClick;
     }
 }
