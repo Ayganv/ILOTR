35955d523101d3411c62e9393510fd28db859207
Virginia Baroni <virg.baroni@gmail.com>
2020-10-20 12:31:45 +0200

    added gold text and objects, canvas

---
+++ Assets/Scripts/GoldManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GoldManager : MonoBehaviour
+{
+    public int goldAmount;
+
+    public Text goldAmountText;
+
+    void Update()
+    {
+        this.goldAmountText.text = this.goldAmount.ToString("0 Gold");
+    }
+}
