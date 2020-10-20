d56b566a82db158b23d7a496ab341e134ab9363e
Virginia Baroni <virg.baroni@gmail.com>
2020-10-20 15:02:00 +0200

    added golden press, all working except generation per second

--- Assets/Scripts/GoldManager.cs
+++ Assets/Scripts/GoldManager.cs
@@ -4,6 +4,11 @@ using UnityEngine.UI;
 public class GoldManager : MonoBehaviour {
     public int goldAmountPerClick = 5;
     public Text goldAmountText;
+    
+    //gold press only
+    private int goldPressCost = 100;
+    public int goldPressAmount;
+    public Text goldPress;
 
     public int GoldAmount {
         get => PlayerPrefs.GetInt("Gold", 1);
@@ -15,6 +20,12 @@ public class GoldManager : MonoBehaviour {
 
     void UpdateGoldAmountLabel() {
         this.goldAmountText.text = this.GoldAmount.ToString("0 Gold");
+    }
+
+    void UpdateGoldPressAmount()
+    {
+        this.goldPressAmount +=1;
+        this.goldPress.text = this.goldPressAmount.ToString("0 Gold Press");
     }
 
     void Start() {
@@ -29,5 +40,16 @@ public class GoldManager : MonoBehaviour {
 
     public void ProduceGold() {
         this.GoldAmount += this.goldAmountPerClick;
+    }
+
+    public void BuyGoldPress()
+    {
+        if (GoldAmount >= goldPressCost)
+        {
+            this.GoldAmount -= goldPressCost;
+            UpdateGoldPressAmount();
+            UpdateGoldAmountLabel();
+        }
+        
     }
 }
